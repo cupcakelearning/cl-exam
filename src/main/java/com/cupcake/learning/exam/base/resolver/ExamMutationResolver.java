c8909b36bd41ad9272dbfb32b4075f698bbb8c5f
Fu Qiang Wong <fuqiang20@gmail.com>
2021-09-21 23:10:07 +0800

    Revert "Add exam delete"
    This reverts commit 6376df3219763ffc6e74fe701c2fec9cf7191d55.

--- src/main/java/com/cupcake/learning/exam/base/resolver/ExamMutationResolver.java
+++ src/main/java/com/cupcake/learning/exam/base/resolver/ExamMutationResolver.java
@@ -1,31 +1,30 @@
 package com.cupcake.learning.exam.base.resolver;
 
-import com.cupcake.learning.exam.auth.repository.UserRepository;
 import com.cupcake.learning.exam.base.model.entity.Exam;
-import com.cupcake.learning.exam.base.model.input.ExamInput;
+import com.cupcake.learning.exam.base.model.entity.ExamQuestion;
 import com.cupcake.learning.exam.base.repository.ExamQuestionRepository;
-import com.cupcake.learning.exam.base.repository.ExamRepository;
 import com.cupcake.learning.exam.util.PatchModelMapper;
+import com.cupcake.learning.exam.auth.repository.UserRepository;
+import com.cupcake.learning.exam.base.model.input.ExamInput;
+import com.cupcake.learning.exam.base.repository.ExamRepository;
 import graphql.kickstart.tools.GraphQLMutationResolver;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Component
 public class ExamMutationResolver implements GraphQLMutationResolver {
     private final PatchModelMapper mapper;
     private final UserRepository userRepository;
-    private final ExamQuestionRepository examQuestionRepository;
     private final ExamRepository examRepository;
 
     public ExamMutationResolver(PatchModelMapper mapper,
                                 UserRepository userRepository,
-                                ExamQuestionRepository examQuestionRepository,
                                 ExamRepository examRepository) {
         this.mapper = mapper;
         this.userRepository = userRepository;
-        this.examQuestionRepository = examQuestionRepository;
         this.examRepository = examRepository;
     }
 
@@ -47,17 +46,6 @@ public class ExamMutationResolver implements GraphQLMutationResolver {
                 .orElseThrow(() -> new RuntimeException("Unable to find given exam"));
         mapper.map(input, exam);
         return examRepository.save(exam);
-    }
-
-    // TODO: Refund if someone bought the exam + if exam is part of book
-    @Transactional
-    public UUID deleteExam(UUID id, UUID authorId) {
-        var exam = examRepository.findByIdAndAuthorId(id, authorId)
-                .orElseThrow(() -> new RuntimeException("Unable to find given exam"));
-
-        var examQuestions = examQuestionRepository.findByIdExamId(id);
-        examQuestionRepository.deleteAll(examQuestions);
-        return id;
     }
 
     public Exam publishExam(UUID id, UUID authorId) {
